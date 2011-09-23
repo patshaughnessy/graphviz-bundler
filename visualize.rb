ee2d2a6da8131d090fc95f42e906141f595f6418
Pat Shaughnessy <pat@patshaughnessy.net>
2011-09-23 10:20:30 -0400

    more

--- visualize.rb
+++ visualize.rb
@@ -5,27 +5,40 @@ require 'graphviz'
 
 class AssociationsGraph
 
-  def generate(gems, file_name)
+  def generate_one(gems, file_name)
     puts 'Generating graph...'
     graph_viz = GraphViz::new('Gemfile', {:concentrate => true, :normalize => true, :nodesep => 0.55})
     graph_viz.edge[:fontname] = graph_viz.node[:fontname] = 'Arial, Helvetica, SansSerif'
     graph_viz.edge[:fontsize] = 12
 
     nodes = {}
+
+    activated_nodes = []
+    activated_nodes[0] = graph_viz.add_node('Activated', { :shape => 'box3d', :fontsize => 16, :style => 'filled', :fillcolor => '#B9B9D5'} )
+
+    activated_count = 1
     gems[:activated].each do |gem|
-      nodes[gem] = graph_viz.add_node(gem, { :shape => 'box3d', :fontsize => 16, :style => 'filled', :fillcolor => '#B9B9D5'} )
+      activated_nodes[activated_count] = graph_viz.add_node(gem, { :fontsize => 16, :style => 'filled', :fillcolor => '#FFFFFF'} )
+      graph_viz.add_edge( activated_nodes[activated_count-1], activated_nodes[activated_count], { :weight => 2 } )
+      activated_count += 1
     end
 
-#    each_model do |model|
-#      name = model.to_s
-#      model.reflect_on_all_associations.each do |assoc|
-#        graph_viz.add_edge( models[name], models[assoc.name.to_s.singularize.titleize], { :weight => 2 } )
-#      end
-#    end
+    dependency_nodes = []
+    dependency_nodes[0] = graph_viz.add_node(gems[:attempting][0], { :shape => 'box3d', :fontsize => 16, :style => 'filled', :fillcolor => '#B9B9D5'} )
+
+    dependency_count = 1
+    gems[:dependencies].each do |gem|
+      dependency_nodes[dependency_count] = graph_viz.add_node(gem, { :fontsize => 16, :style => 'filled', :fillcolor => '#FFFFFF'} )
+      graph_viz.add_edge( dependency_nodes[dependency_count-1], dependency_nodes[dependency_count], { :weight => 2 } )
+      dependency_count += 1
+    end
 
     graph_viz.output( :png => file_name )
 
     puts 'Done.'
+  end
+
+  def generate_two(gems, file_name)
   end
 
   def initialize
@@ -40,7 +53,6 @@ class AssociationsGraph
 
   def process(lines)
     next_count
-    puts "Iteration: #{count}; process #{lines.size} lines. "
 
     gems = Hash.new { |hash, key| hash[key] = [] }
     type = :none
@@ -64,7 +76,9 @@ class AssociationsGraph
         end
       end
     end
-    generate(gems, "test#{count}.png")
+    puts "Iteration: #{count}; process #{lines.size} lines. Activate count: #{gems[:activated].size}"
+    generate_one(gems, "first#{count}.png") if count < 10
+    generate_two(gems, "second#{count}.png") if count < 10
   end
 end
 
